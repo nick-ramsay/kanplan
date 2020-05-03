91bf3869f87edbf08ee2bb4ac8561bf9b11c078e
Nick Ramsay <NickRamsay1989@gmail.com>
2020-05-03 20:26:57 +1000

    Added new swimlane column

--- public/assets/js/kanban.js
+++ public/assets/js/kanban.js
@@ -4,5 +4,16 @@ $(document).ready(
         $(".swimlane-card").draggable();
         $(".swimlane").sortable();
         $(".swimlane").disableSelection();
-    }   
+        console.log(Date.now());
+    }
 );
+
+function createNewCard() {
+    var uuid1 = Date.now();
+    var uuid2 = Math.floor(Math.random()*100000);
+    var uuid3 = Math.floor(Math.random()*100000);
+    
+    var unique_id = uuid1 + "-" + uuid2 + "-" + uuid3;
+    
+    console.log(unique_id);
+}
