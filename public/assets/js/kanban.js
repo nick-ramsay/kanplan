1bbb83c6dbdc1856fe9290d07abb03a6efd1ab64
Nick Ramsay <NickRamsay1989@gmail.com>
2020-05-10 14:01:50 +1000

    Cards now sortable with placeholder logic and formatting

--- public/assets/js/kanban.js
+++ public/assets/js/kanban.js
@@ -1,19 +1,23 @@
 $(document).ready(
     function () {
         console.log("Testing kanban!");
-        $(".swimlane-card").draggable();
-        $(".swimlane").sortable();
-        $(".swimlane").disableSelection();
+        
+        $("#sortable").sortable({
+            placeholder: "ui-state-highlight"
+        });
+        
+        $("#sortable").disableSelection();
+        
         console.log(Date.now());
     }
 );
 
 function createNewCard() {
     var uuid1 = Date.now();
-    var uuid2 = Math.floor(Math.random()*100000);
-    var uuid3 = Math.floor(Math.random()*100000);
-    
+    var uuid2 = Math.floor(Math.random() * 100000);
+    var uuid3 = Math.floor(Math.random() * 100000);
+
     var unique_id = uuid1 + "-" + uuid2 + "-" + uuid3;
-    
+
     console.log(unique_id);
 }
